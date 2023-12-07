67729fe6d887fd191b4ffc6c17c3a6311ba1af38
Guru Saran Satsangi Peddinti <107044770+GurusaranSP@users.noreply.github.com>
2023-12-07 18:01:17 +0530

    text editor.py

--- text editor.py
+++ text editor.py
@@ -9,33 +9,32 @@ text = Text(rootwin)
 text.grid()
 
 
-def savedb():
+def save_to_file():
     global text
     txt = text.get("1.0", "end-1c")
     savelocation = tkinter.filedialog.asksaveasfile()
-    print(savelocation.name)
-    file1 = open(savelocation.name, "w+")
-    file1.write(txt)
-    file1.close()
-button=Button(rootwin,text="save", command=savedb)
-button.grid()
+    if savelocation:
+        print(savelocation.name)
+        with open(savelocation.name, "w+") as file1:
+            file1.write(txt)
 
-def fontHelvetica():
+def font_helvetica():
     global text
     text.config(font="Helvetica")
 
-def fontCourier():
+def font_courier():
     global text
     text.config(font="Courier")
-font=Menubutton(rootwin,text="font")
-font.grid()
-font.menu=Menu(font,tearoff=0)
-font["menu"]=font.menu
-Helvetica=IntVar()
-Arial=IntVar()
-Times=IntVar()
-Courier=IntVar()
 
-font.menu.add_checkbutton(label="Courier",variable=Courier,command=fontCourier)
-font.menu.add_checkbutton(label="Helvetica",variable=Helvetica,command=fontHelvetica)
+button_save = Button(rootwin, text="save", command=save_to_file)
+button_save.grid()
+
+font_menu = Menu(rootwin, tearoff=0)
+font_menu.add_checkbutton(label="Courier", variable=Courier, command=font_courier)
+font_menu.add_checkbutton(label="Helvetica", variable=Helvetica, command=font_helvetica)
+
+menu_bar = Menu(rootwin)
+menu_bar.add_cascade(label="font", menu=font_menu)
+rootwin.config(menu=menu_bar)
+
 rootwin.mainloop()
